68ea8a53f6e24ce810cdfbe3e495ce58c3e65148
Jufraas <jufra1605@gmail.com>
2026-09-21 21:16:53 -0500

    Commit_cambios

--- Calculadora.java
+++ Calculadora.java
@@ -15,7 +15,10 @@ public class Calculadora {
                 + "\n 1: Suma"
                 + "\n 2: Resta"
                 + "\n 3: Multiplicacion"
-                + "\n 4: Division");
+                + "\n 4: Division"
+                + "\n 5: Potencia"
+                + "\n 6: Raiz cuadrada"
+                + "\n 7: Modulo");
         op = leer.nextInt();
 
         switch (op) {
@@ -37,6 +40,54 @@ public class Calculadora {
                     System.out.println("El resultado es: " + res);
                 } else {
                     System.out.println("No se puede dividir entre 0 :(");
+                }
+                break;
+            case 5:
+                res = (float) Math.pow(num1, num2);
+                System.out.println("El resultado es: " + res);
+                break;
+            case 6:
+                if (num1 >= 0) {
+                    res = (float) Math.sqrt(num1);
+                    System.out.println("La raiz de " + num1 + " es: " + res);
+                } else {
+                    System.out.println("No existe raiz cuadrada de un numero negativo");
+                }
+                break;
+            case 7:
+                if (num2 != 0) {
+                    res = num1 % num2;
+                    System.out.println("El residuo es: " + res);
+                } else {
+                    System.out.println("No se puede sacar modulo entre 0");
+                }
+                break;
+            case 8:
+                res = Math.sin(num1 * Math.PI / 180);
+                System.out.println("El seno de " + num1 + " es: " + res);
+                break;
+            case 9:
+                res = Math.cos(num1 * Math.PI / 180);
+                System.out.println("El coseno de " + num1 + " es: " + res);
+                break;
+            case 10:
+                if (num1 % 180 == 90 || num1 % 180 == -90) {
+                    System.out.println("El tangente de " + num1 + " es indefinido");
+                } else {
+                    res = Math.tan(num1 * Math.PI / 180);
+                    System.out.println("El tangente de " + num1 + " es: " + res);
+                }
+                break;
+            case 11:
+                res = num1 * (num2 / 100);
+                System.out.println(num2 + "% de " + num1 + " es: " + res);
+                break;
+            case 12:
+                if (num1 > 0) {
+                    res = Math.log10(num1);
+                    System.out.println("El logaritmo de " + num1 + " es: " + res);
+                } else {
+                    System.out.println("El logaritmo solo existe para numeros mayores que 0");
                 }
                 break;
             default:
